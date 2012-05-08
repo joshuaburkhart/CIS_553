9e24a43027d0b3ed527ea7825215a502d22c5563
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-05-08 02:56:35 -0700

    mining data!!

--- Assignment3/fp.rb
+++ Assignment3/fp.rb
@@ -1,20 +1,33 @@
+#!/usr/local/bin/ruby
+#Usage: fp.rb <filename> <support>
+#Example: ./fp.rb ~/tmp/FoodMart.xls 20
+
 require 'rubygems'
 require 'fp_growth'
+require 'parseexcel'
 
-def fp
-min_support = 2
-transactions = [
-	["milk ","honey "],
-	["bread "],
-	["milk ","bread "],
-	["milk ","bread "]
-]
-items = FpGrowth::FpTree.get_items(transactions)
-f = FpGrowth::FpTree.new(min_support,items,transactions)
-puts "\nTree Structure\n\n"
-puts f
-puts "\nFrequent Itemsets\n\n"
-puts f.fp_growth
+def parsefile(filename)
+  workbook = Spreadsheet::ParseExcel.parse(filename)
+  worksheet = workbook.worksheet(0)
+  headers = worksheet.first
+  sheet_array = Array.new
+  worksheet.each(0) { |row|
+    row_array = Array.new
+    row.each_with_index { |cell,i|
+      if(cell.to_i == 1)
+        row_array << headers[i]
+      end
+    }
+    sheet_array << row_array
+  }
+  sheet_array
 end
 
-fp
+def fp(transactions, min_support)
+  items = FpGrowth::FpTree.get_items(transactions)
+  f = FpGrowth::FpTree.new(min_support,items,transactions)
+  puts "\nFrequent Itemsets\n\n"
+  puts f.fp_growth
+end
+
+fp(parsefile(ARGV[0]), Integer(ARGV[1]))
