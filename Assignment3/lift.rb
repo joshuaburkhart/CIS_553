8833d9c2932b27bd96c3db2cf9c893b28585b6bc
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-05-09 20:49:26 -0700

    lift is looking good

--- Assignment3/lift.rb
+++ Assignment3/lift.rb
@@ -1,17 +1,24 @@
-#Usage: ruby fp.rb <filename> <minimum support> <minimum confidence>
+#Usage: ruby lift.rb <filename> <minimum support count>
 #
-#Example: ruby fp.rb ~/tmp/FoodMart.xls 5 .70
+#Example: ruby lift.rb ~/tmp/FoodMart.xls 20
 
 require 'rubygems'
 require 'fp_growth'
 require 'parseexcel'
+
+class FpGrowth::PrefixPath
+  attr :lift, true
+  def to_s
+    "[#{@array} :#{support} :#{lift}]"
+  end
+end
 
 def parsefile(filename)
   workbook = Spreadsheet::ParseExcel.parse(filename)
   worksheet = workbook.worksheet(0)
   headers = worksheet.first
   sheet_array = Array.new
-  worksheet.each(0) { |row|
+  worksheet.each(1) { |row|
     row_array = Array.new
     row.each_with_index { |cell,i|
       if(cell.to_i == 1)
@@ -24,19 +31,46 @@ def parsefile(filename)
 end
 
 def fp(transactions, min_support, min_lift)
+  t_size = Float(transactions.size)
   items = FpGrowth::FpTree.get_items(transactions)
   f = FpGrowth::FpTree.new(min_support,items,transactions)
   frequent_itemsets = f.fp_growth
   frequent_itemsets.each { |itemset|
-      puts "size of itemset = #{itemset.size}"
-      puts "content of itemset #{itemset}"
-      itemset.each { |item|
-	puts "  count of item = #{item.count}"
-        puts "  size of item = #{item.size}"
-  	puts "  content of item = #{item}"
-      }
+      if(itemset.size > 1)
+	#puts "found a frequent itemset of 2 or more"
+        #puts "class of itemset = #{itemset.class}"
+        #puts "size of itemset = #{itemset.size}"
+        #puts "content of itemset = #{itemset}"
+        #puts "support of itemset = #{itemset.support}"
+       	partial_support = 1.0
+        itemset.each { |item|
+          #puts "  item = #{item}"
+	  #puts "  class of item = #{item.class}"
+	  #puts "  size of item = #{item.size}"
+	  #puts "  item.to_a = #{item.to_a}"
+          idx = frequent_itemsets.index { |candidate|
+		  #puts "    comparing to #{candidate}"
+		  #puts "    candidate.class = #{candidate.class}"
+		  #puts "    item.class = #{item.class}"
+		  #puts "    candidate.size = #{candidate.size}"
+		  #puts "    candidate.size == 1 ? #{candidate.size == 1}"
+		  #puts "    candidate.to_a #{candidate.to_a}"
+		  #puts "    candidate.to_a.class #{candidate.to_a.class}"
+		  #puts "    candidate.to_a == #{item.to_a} ? #{candidate.to_a == item.to_a}"
+		  #puts "    candidate.to_s = #{candidate.to_s}"
+		  #puts "    candidate.to_s.class = #{candidate.to_s.class}"
+		  #puts "    candidate.to_s.eql? #{item.to_s} ? #{candidate.to_s.eql? item.to_s}"
+		  candidate.size == 1 && candidate.to_a == item.to_a
+	  } 
+	  partial_support *= (frequent_itemsets[idx].support / t_size)
+	}
+	puts "setting #{itemset}'s lift to #{Float(itemset.support) / t_size} / #{partial_support}"
+	itemset.lift = (Float(itemset.support) / t_size) / partial_support
+     else
+       itemset.lift = 0
+     end
    }
-  puts frequent_itemsets
+  puts frequent_itemsets.sort { |itemset_1,itemset_2| itemset_1.lift <=> itemset_2.lift}
 end
 
-fp(parsefile(ARGV[0]), Integer(ARGV[1]), Float(ARGV[2]))
+fp(parsefile(ARGV[0]), Integer(ARGV[1]))
