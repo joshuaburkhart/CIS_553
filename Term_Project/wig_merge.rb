143c1803bee6ff506c406e7e27a863fb3d6278da
Joshua Burkhart <jburkhar@uoregon.edu>
2012-05-28 17:25:44 -0700

    wig_merge works

--- Term_Project/wig_merge.rb
+++ Term_Project/wig_merge.rb
@@ -1,66 +1,85 @@
 #Usage: ruby wig_merge.rb <reference filename> <auxillary filename 1> <auxillary filename 2>
 #Example: ruby wig_merge.rb ESX0000001760.wig ESX0000001748.wig ESX0000003301.wig
 
+merge_handle = File.open('merge_test.txt','w')
+
 ref_filename = ARGV[0]
-print "\t#{ref_filename}\t"
+merge_handle.print "\t#{ref_filename}\t"
 
 aux_files = Array.new
 aux_l_buf = Array.new
+
 (1..ARGV.length - 1).each do |i|
-	print "#{ARGV[i]}\t"
+	merge_handle.print "#{ARGV[i]}\t"
 	aux_files << File.new(ARGV[i])
 	aux_l_buf << "track"
 end
-puts
 
-def valid(line,chrm,limit)
-	within_limit = false
+merge_handle.puts
+
+def valid(line,last,chrm,limit)
+	within_limit = false 
 	if(line.match(/^#{chrm}-.*[' '].*$/))
-		line_pos = line.match(/^#{chrm}-(.*?)[' '].*$/)[1]
+		line_pos = line.match(/^#{chrm}-(.*)[' '].*$/)[1]
 		within_limit = (Integer(line_pos) <= Integer(limit))
-		#puts "line_pos: #{line_pos}, limit: #{limit}, within_limit: #{within_limit}"
+	elsif(line.match(/^track/))
+		within_limit = true
+	elsif(line.match(/^variableStep/))
+		within_limit = true
+	elsif(last != chrm)
+		within_limit = true
 	end
-	return line.match(/^track/) || line.match(/^variableStep/) || within_limit
+	return within_limit
 end
+
+ref_chrm = "default"
+last_ref = "default"
+ref_start = -1
+ref_label = "no-label"
 
 File.open(ref_filename,'r') do |ref_file|
 	while ref_line = ref_file.gets
-		ref_chrm = "default"
-		ref_start = -1
-		ref_label = "no-label"
 		values = Array.new
-
-puts "ref_line: #{ref_line}"
 
 		if(ref_line.match(/^track/))
 			#print ref_line
 		elsif(ref_line.match(/^variableStep/))
 			#print ref_line
 		elsif(ref_line.match(/^chr.*?-.*[' '].*$/))
+			last_ref = ref_chrm
 			ref_chrm = ref_line.match(/^(chr.*?)-.*$/)[1]
-puts "ref_chrm: #{ref_chrm}"
 			ref_start = ref_line.match(/^#{ref_chrm}-(.*)[' '].*$/)[1]
 			ref_label = "#{ref_chrm}-#{ref_start}"
+			#puts "trying to match #{ref_label}"
 			values << ref_line.match(/^#{ref_label}[' '](.*)$/)[1]
 			aux_files.each_index do |i|
-				while (cur_line=aux_l_buf[i]) && (valid(cur_line,ref_chrm,ref_start))
+				#puts "comparing to #{aux_l_buf[i]}"
+				while((cur_line = aux_l_buf[i]) && (valid(cur_line,last_ref,ref_chrm,ref_start)))
 					if(cur_line.match(/^#{ref_label}[' '].*?$/))
+						#puts "* matched with #{cur_line}"
 						values << cur_line.match(/^#{ref_label}[' '](.*)$/)[1]
+						break
 					end
 					aux_l_buf[i] = aux_files[i].gets
+					#puts "now comparing to #{aux_l_buf[i]}"
 				end
 			end
 
 			if(values.length == (aux_files.length + 1))
-				print "#{ref_label}\t"
+				#print "outputting #{ref_label}"
+				merge_handle.print "#{ref_label}\t"
 				values.each do |i|
-					print "#{i}\t"
+					#print " #{i}"
+					merge_handle.print "#{i}\t"
 				end
-				puts
+				#puts
+				merge_handle.puts
 			end
 		end		
 	end
 end
+
+merge_handle.close
 
 aux_files.each do |aux_file|
 	aux_file.close
